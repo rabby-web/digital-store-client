b4bee3361324ba0a6420bb79078f7373c72e38e8
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-18 19:38:58 +0600

    server data load from mongodb

--- src/page/Brand/Brand.jsx
+++ src/page/Brand/Brand.jsx
@@ -1,9 +1,13 @@
 import { useEffect, useState } from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import BrandCarousel from "../BrandCarousel/BrandCarousel";
+import { AiOutlineStop } from "react-icons/ai";
+import BrandGallery from "../BrandGallery/BrandGallery";
 
 const Brand = () => {
   const [gBrand, setGBrand] = useState({});
+  const [bProducts, setBProducts] = useState([]);
+  const [flteredProducts, setFilterProducts] = useState([]);
 
   const totalBrand = useLoaderData();
   const brand_name = useParams();
@@ -17,12 +21,47 @@ const Brand = () => {
 
     setGBrand(filterBrand);
   }, [totalBrand, brand_name]);
+  useEffect(() => {
+    fetch("http://localhost:5000/products")
+      .then((res) => res.json())
+      .then((data) => setBProducts(data));
+  }, []);
 
+  useEffect(() => {
+    const filterData = bProducts.filter(
+      (product) =>
+        product.brand.toLowerCase() == brand_name.brand_name.toLowerCase()
+    );
+    setFilterProducts(filterData);
+  }, [bProducts, brand_name]);
+  {
+    /* <BrandCarousel gBrand={gBrand}></BrandCarousel> */
+  }
   return (
     <div>
-      <h2>Brand: {gBrand.brand_name}</h2>
       <div>
         <BrandCarousel gBrand={gBrand}></BrandCarousel>
+      </div>
+      <div>
+        <div>
+          {flteredProducts.length > 0 ? (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 my-20">
+              {flteredProducts.map((oneBrand) => (
+                <BrandGallery
+                  key={oneBrand._id}
+                  oneBrand={oneBrand}
+                ></BrandGallery>
+              ))}
+            </div>
+          ) : (
+            <div className="flex justify-center items-center h-[50vh] text-3xl font-bold">
+              <div>
+                <AiOutlineStop className="text-5xl text-red-600 mx-auto mb-5"></AiOutlineStop>
+                <h1>No Product Here</h1>
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
---
+++ src/page/BrandGallery/BrandGallery.jsx
@@ -0,0 +1,78 @@
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const BrandGallery = ({ oneBrand }) => {
+  const { image, name, brand, type, price, rating } = oneBrand || {};
+
+  return (
+    <div>
+      <div className="relative flex border flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-lg">
+        <div className="relative mx-4 mt-4 overflow-hidden text-white shadow-lg rounded-xl bg-blue-gray-500 bg-clip-border shadow-blue-gray-500/40">
+          <img
+            className="h-52 object-cover"
+            src={image}
+            alt="ui/ux review check"
+          />
+          <div className="absolute inset-0 w-full h-full to-bg-black-10 bg-gradient-to-tr from-transparent via-transparent to-black/60"></div>
+        </div>
+        <div className="px-6 mt-4">
+          <div className="flex items-center justify-between mb-3">
+            <h5 className="block font-sans text-2xl   antialiased font-bold leading-snug tracking-normal text-blue-gray-900">
+              {name}
+            </h5>
+            <p className="flex items-center gap-1.5 font-sans text-base leading-relaxed text-blue-gray-900 antialiased font-semibold">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 24 24"
+                fill="currentColor"
+                aria-hidden="true"
+                className="-mt-0.5 h-5 w-5 text-yellow-500"
+              >
+                <path
+                  fillRule="evenodd"
+                  d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z"
+                  clipRule="evenodd"
+                ></path>
+              </svg>
+              {rating}
+            </p>
+          </div>
+
+          <div className="flex justify-between text-lg text-sky-800">
+            <h1 className="font-semibold">Brand: {brand}</h1>
+            <h1 className="font-semibold">Price: {price} $</h1>
+          </div>
+
+          <div className="my-2">
+            <h1 className="font-semibold text-lg">Type: {type}</h1>
+          </div>
+        </div>
+        <div className="p-4 flex justify-between">
+          <Link>
+            <button
+              type="button"
+              className="text-white bg-sky-700 hover:bg-sky-800 border border-sky-700 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Details
+            </button>
+          </Link>
+
+          <Link>
+            <button
+              type="button"
+              className="text-sky-700 hover:text-white border border-sky-700 hover:bg-sky-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
+            >
+              Update
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+BrandGallery.propTypes = {
+  oneBrand: PropTypes.object.isRequired,
+};
+
+export default BrandGallery;
