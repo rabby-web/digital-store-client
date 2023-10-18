5d70e887746fdee4cffdb97d2e275bd8e0154ee4
rabby-web <rabby.webdeveloper@gmail.com>
2023-10-18 19:31:37 +0600

    caoursel add

--- src/page/BrandCarousel/BrandCarousel.jsx
+++ src/page/BrandCarousel/BrandCarousel.jsx
@@ -4,11 +4,11 @@ const BrandCarousel = ({ gBrand }) => {
   console.log(gBrand.slider_1);
   const { slider_1, slider_2, slider_3 } = gBrand || {};
   return (
-    <div>
-      <div className="carousel w-full h-[100vh] p-2 rounded-xl">
-        <div id="slide1" className="carousel-item relative w-full m-2">
-          <img src={slider_1} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+    <div className="w-3/4 mx-auto h-[60vh]">
+      <div className="carousel w-full">
+        <div id="slide1" className="carousel-item relative w-full">
+          <img src={slider_1} className="w-full object-cover h-[60vh]" />
+          <div className="absolute flex justify-between left-5 right-5 items-center h-[60vh]">
             <a href="#slide3" className="btn btn-circle">
               ❮
             </a>
@@ -18,8 +18,8 @@ const BrandCarousel = ({ gBrand }) => {
           </div>
         </div>
         <div id="slide2" className="carousel-item relative w-full">
-          <img src={slider_2} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <img src={slider_2} className="w-full object-cover h-[60vh]" />
+          <div className="absolute flex justify-between left-5 right-5 items-center h-[60vh]">
             <a href="#slide1" className="btn btn-circle">
               ❮
             </a>
@@ -29,8 +29,8 @@ const BrandCarousel = ({ gBrand }) => {
           </div>
         </div>
         <div id="slide3" className="carousel-item relative w-full">
-          <img src={slider_3} className="w-full" />
-          <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+          <img src={slider_3} className="w-full object-cover h-[60vh]" />
+          <div className="absolute flex justify-between left-5 right-5 items-center h-[60vh]">
             <a href="#slide2" className="btn btn-circle">
               ❮
             </a>
